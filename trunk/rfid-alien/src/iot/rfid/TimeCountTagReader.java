e6f237628394684276c03d110aa877c94ee7d010
lukasmeirelles <lukasmeirelles@5305ca0a-90bc-559b-e43d-155ca727e441>
2011-10-27 11:00:08 +0000

    Criado diretórios para documentos e adicionadas classes de teste

---
+++ trunk/rfid-alien/src/iot/rfid/TimeCountTagReader.java
@@ -0,0 +1,67 @@
+package iot.rfid;
+
+import iot.rfid.config.Configuration;
+import iot.rfid.reader.RFIDReaderManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alien.enterpriseRFID.tags.Tag;
+
+public class TimeCountTagReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++)
+			execute();
+	}
+
+	private static void execute() {
+		// if (args.length != 1) {
+		// System.out
+		// .println("Informe a quantidade de segundos para leitura.");
+		// return;
+		// }
+
+		Integer timeCount = 10;
+		if (timeCount == null || timeCount < 1) {
+			System.out.println("Tempo de leitura deve ser maior que zero");
+			return;
+		}
+
+		RFIDReaderManager readerManager = new RFIDReaderManager();
+
+		readerManager.createReaderByIp(Configuration.DCC_READER_IP);
+		readerManager.openConnection();
+
+		long startTime = System.currentTimeMillis();
+
+		Map<String, Integer> tagReadMap = new HashMap<String, Integer>();
+		int count = 0;
+		while (System.currentTimeMillis() - startTime < timeCount * 1000) {
+			Tag[] tags = readerManager.getReadTags();
+			if (tags != null) {
+				for (Tag tag : tags) {
+					Integer tagCount = tagReadMap.get(tag.getTagID());
+					if (tagCount == null) {
+						tagReadMap.put(tag.getTagID(), 1);
+					} else {
+						tagReadMap.put(tag.getTagID(), ++tagCount);
+					}
+				}
+			}
+		}
+
+		System.out.println(String.format("Quantidade de tags lidas: %s",
+				tagReadMap.keySet().size()));
+		for (String key : tagReadMap.keySet()) {
+			count += tagReadMap.get(key);
+		}
+		System.out.println(String.format("Total de leituras : %s", count));
+
+		readerManager.closeConnection();
+	}
+
+}
